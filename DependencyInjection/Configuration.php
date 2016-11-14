84b5e89165595a7b460794cb279d9a7e1403fba3
Tim Goudriaan <tim@codedmonkey.com>
2016-11-14 21:46:57 +0100

    Added the bundle extension and configuration definition

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,116 @@
+<?php
+/*
+ * This file is part of the Harmony package.
+ *
+ * (c) Tim Goudriaan <tim@harmony-project.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Harmony\Bundle\ModularBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * Configuration
+ *
+ * This class validates and merges configuration for the bundle.
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder;
+        $root = $treeBuilder->root('harmony_modular');
+
+        $root
+            ->children()
+                ->scalarNode('module_class')->defaultNull()->end()
+                ->scalarNode('module_identifier')->defaultValue('id')->end()
+            ->end()
+        ;
+
+        $this->addRoutePrefixSection($root);
+        $this->addRouterSection($root);
+        $this->addServicesSection($root);
+
+        return $treeBuilder;
+    }
+
+    private function addRoutePrefixSection(ArrayNodeDefinition $root)
+    {
+        $root
+            ->children()
+                ->arrayNode('route_prefix')
+                    ->info('route prefix configuration')
+                    ->addDefaultsIfNotSet()
+                    ->beforeNormalization()
+                        ->ifString()
+                            ->then(function($value) { return [
+                                'prefix'       => $value,
+                                'defaults'     => [],
+                                'requirements' => [],
+                            ]; })
+                    ->end()
+                    ->children()
+                        ->scalarNode('prefix')->defaultValue('/module')->end()
+                        ->arrayNode('defaults')
+                            ->prototype('array')
+                                ->children()
+                                    ->scalarNode('name')->end()
+                                    ->scalarNode('default')->end()
+                                ->end()
+                            ->end()
+                        ->end()
+                        ->arrayNode('requirements')
+                            ->useAttributeAsKey('name')
+                            ->prototype('array')
+                                ->children()
+                                    ->scalarNode('name')->end()
+                                    ->scalarNode('requirement')->end()
+                                ->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+    }
+
+    private function addRouterSection(ArrayNodeDefinition $root)
+    {
+        $root
+            ->children()
+                ->arrayNode('router')
+                    ->info('router configuration')
+                    ->children()
+                    ->scalarNode('resource')->isRequired()->end()
+                    ->scalarNode('resource_type')->defaultNull()->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+    }
+
+    private function addServicesSection(ArrayNodeDefinition $root)
+    {
+        $root
+            ->children()
+                ->arrayNode('service')
+                    ->info('services configuration')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->scalarNode('module_manager')->defaultNull()->end()
+                        ->scalarNode('provider')->defaultNull()->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+    }
+}
---
+++ DependencyInjection/HarmonyModularExtension.php
@@ -0,0 +1,99 @@
+<?php
+/*
+ * This file is part of the Harmony package.
+ *
+ * (c) Tim Goudriaan <tim@harmony-project.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Harmony\Bundle\ModularBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+/**
+ * HarmonyModularExtension
+ *
+ * Loads and manages the bundle configuration.
+ */
+class HarmonyModularExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $config = $this->processConfiguration(new Configuration(), $configs);
+
+        // disable the bundle if no module class is specified
+        if (!$config['module_class']) {
+            return;
+        }
+
+        // Load services
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+
+        // Set parameters
+        $container->setParameter('harmony_modular.module_class', $config['module_class']);
+        $container->setParameter('harmony_modular.module_identifier', $config['module_identifier']);
+
+        $container->setParameter('harmony_modular.router.resource', $config['router']['resource']);
+        $container->setParameter('harmony_modular.router.resource_type', $config['router']['resource_type'] ?: 'yaml');
+
+        $this->setupRoutePrefix($config, $container);
+        $this->setupServices($config, $container);
+    }
+
+    private function setupRoutePrefix(array $config, ContainerBuilder $container)
+    {
+        $config = $config['route_prefix'];
+
+        $container->setParameter('harmony_modular.route_prefix.path', $config['prefix']);
+
+        if ($config['defaults']) {
+            $defaults = [];
+            foreach ($config['defaults'] as $default) {
+                $defaults[$default['name']] = $default['default'];
+            }
+
+            $container->setParameter('harmony_modular.route_prefix.defaults', $defaults);
+        }
+        else {
+            $container->setParameter('harmony_modular.route_prefix.defaults', []);
+        }
+
+        if ($config['requirements']) {
+            $requirements = [];
+            foreach ($config['requirements'] as $requirement) {
+                $requirements[$requirement['name']] = $requirement['requirement'];
+            }
+
+            $container->setParameter('harmony_modular.route_prefix.requirements', $requirements);
+        }
+        else {
+            $container->setParameter('harmony_modular.route_prefix.requirements', []);
+        }
+    }
+
+    private function setupServices(array $config, ContainerBuilder $container)
+    {
+        // Define a different default module manager when using the default StaticModule class
+        $static = 'Harmony\Component\ModularRouting\Model\StaticModule' == $config['module_class'];
+        $defaultManagerService = !$static ? 'harmony_modular.module_manager.doctrine' : 'harmony_modular.module_manager.static';
+
+        // Set module manager service alias
+        $managerService = $config['service']['module_manager'] ?: $defaultManagerService;
+
+        $container->setAlias('harmony_modular.module_manager', $managerService);
+
+        // Set provider service alias
+        $providerService = $config['service']['provider'] ?: 'harmony_modular.provider.segment';
+
+        $container->setAlias('harmony_modular.provider', $providerService);
+    }
+}
