816185a80e0fbbd98a0a8a563969213d8e6d61af
Tim Goudriaan <tim@codedmonkey.com>
2016-11-14 21:51:08 +0100

    Added the resource resolver compiler pass

---
+++ DependencyInjection/Compiler/MetadataResourceResolverPass.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * This file is part of the Harmony package.
+ *
+ * (c) Tim Goudriaan <tim@harmony-project.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Harmony\Bundle\ModularBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ * MetadataResourceResolverPass
+ *
+ * Scans the services tree for resource loaders and adds them to the resource resolver.
+ */
+class MetadataResourceResolverPass implements CompilerPassInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+        if (false === $container->hasDefinition('harmony_modular.metadata.resource_resolver')) {
+            return;
+        }
+        
+        $definition = $container->getDefinition('harmony_modular.metadata.resource_resolver');
+        
+        foreach ($container->findTaggedServiceIds('harmony_modular.metadata.resource_loader') as $id => $attributes) {
+            $definition->addMethodCall('addLoader', array(new Reference($id)));
+        }
+    }
+}
