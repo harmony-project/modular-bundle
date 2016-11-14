1d1de1098307e151dd6a6bd662498166a495ca32
Tim Goudriaan <tim@codedmonkey.com>
2016-11-14 21:37:15 +0100

    Added bundle class

---
+++ HarmonyModularBundle.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * This file is part of the Harmony package.
+ *
+ * (c) Tim Goudriaan <tim@harmony-project.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Harmony\Bundle\ModularBundle;
+
+use Harmony\Bundle\ModularBundle\DependencyInjection\Compiler;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * HarmonyModularBundle
+ */
+class HarmonyModularBundle extends Bundle
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        
+        $container->addCompilerPass(new Compiler\MetadataResourceResolverPass);
+    }
+}
