1f9d8fa85cc09a1ab063187a9ff5f013de034a94
Tim Goudriaan <tim@codedmonkey.com>
2017-02-24 23:06:26 +0100

    Create twig extension

---
+++ Twig/ModularExtension.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * This file is part of the Harmony package.
+ *
+ * (c) Tim Goudriaan <tim@harmony-project.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Harmony\Bundle\ModularBundle\Twig;
+
+use Harmony\Component\ModularRouting\Manager\ModuleManager;
+
+/**
+ * ModularExtension
+ */
+class ModularExtension extends \Twig_Extension implements \Twig_Extension_GlobalsInterface
+{
+    /**
+     * @var ModuleManager
+     */
+    private $manager;
+
+    /**
+     * @param ModuleManager $manager
+     */
+    public function __construct(ModuleManager $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    /**
+     * @return array
+     */
+    public function getGlobals()
+    {
+        return [
+            'module' => $this->manager->getCurrentModule(),
+        ];
+    }
+}
