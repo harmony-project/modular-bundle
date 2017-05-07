b4299767feec5aa056ef97b865c15539125902a2
Tim Goudriaan <tim@codedmonkey.com>
2017-05-07 06:42:30 +0200

    Move Doctrine subscriber to Doctrine namespace

--- Doctrine/ModularSubscriber.php
+++ Doctrine/ModularSubscriber.php
@@ -1,19 +1,27 @@
 <?php
+/*
+ * This file is part of the Harmony package.
+ *
+ * (c) Tim Goudriaan <tim@harmony-project.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-namespace Harmony\Bundle\ModularBundle\EventListener;
+namespace Harmony\Bundle\ModularBundle\Doctrine;
 
 use Doctrine\Common\EventSubscriber;
-use Harmony\Component\ModularRouting\EventListener\EntitySubscriber as BaseSubscriber;
+use Harmony\Component\ModularRouting\Doctrine\ModularSubscriber as BaseSubscriber;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * EntitySubscriber
+ * ModularSubscriber
  *
  * Handle events regarding modular entities.
  *
  * @author Tim Goudriaan <tim@codedmonkey.com>
  */
-class EntitySubscriber extends BaseSubscriber implements EventSubscriber
+class ModularSubscriber extends BaseSubscriber implements EventSubscriber
 {
     /**
      * @var ContainerInterface
